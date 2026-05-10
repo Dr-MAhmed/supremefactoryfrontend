c1b25402ed139290ad700337d7827b62b3452cbf
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2026-05-10 18:18:56 +0500

    frontendd fixing

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosHeaders } from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:10000/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
