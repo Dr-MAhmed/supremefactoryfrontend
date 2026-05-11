4e9ccbe1efbadb6a73701bddd7a933e67357cced
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2026-05-12 00:57:49 +0500

    fix: update revision for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-f68fa42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.0eb11na4dog"
+    "revision": "0.2kk578fgvj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
